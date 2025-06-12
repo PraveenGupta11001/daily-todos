bf3ea34484daa8841be946aee42052d7253e777d
PraveenGupta11001 <praweengupta11001@gmail.com>
2025-06-12 20:48:52 +0530

    Prepared backend for Render deployment

--- backend/main.py
+++ backend/main.py
@@ -16,6 +16,7 @@ app.add_middleware(
     allow_origins=[
         "http://192.168.29.102:5173",
         "https://daily-todos-alpha.vercel.app/",
+        "https://daily-todos-alpha.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
